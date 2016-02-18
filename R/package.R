0197dab2922d0feb032cf3f0c6e58bfc90e9f12e
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-18 00:04:50 +0000

    Move some utils to separate file

--- R/package.R
+++ R/package.R
@@ -116,31 +116,3 @@ post_process <- function(nodes, edges) {
 is_child <- function(child, parent) {
   substring(child, 1, nchar(parent)) == parent && child != parent
 }
-
-what_expr <- function(expr) {
-  if (is.function(expr)) {
-    "function"
-  } else if (is.call(expr)) {
-    paste0(as.character(expr[[1]]), "()")
-
-  } else {
-    typeof(expr)
-  }
-}
-
-## We need the tryCatch, because the string might contain invalid
-## multi-byte characters. E.g.
-## substring('\x93', 1, 10) and nchar('\x93') both fail
-
-what_atomic <- function(expr) {
-  if (is.character(expr)) {
-    tryCatch(
-      paste0("\"", substring(expr[1], 1, 10), "\""),
-      error = function(e) "\"<string>\""
-    )
-  } else if (is.name(expr)) {
-    as.character(expr)
-  } else {
-    "atomic"
-  }
-}
