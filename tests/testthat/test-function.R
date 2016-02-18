ffd83e20afc216d66228685d4bd14994e5137b48
Gabor Csardi <csardi.gabor@gmail.com>
2016-02-18 23:17:40 +0000

    Test function with argument

--- tests/testthat/test-function.R
+++ tests/testthat/test-function.R
@@ -6,4 +6,6 @@ test_that("function definitions work", {
   f <- function() function() "foo"
   expect_equal(cyclocomp(f), 1)
 
+  f <- function() function(ok) ok
+  expect_equal(cyclocomp(f), 1)
 })
