42cfcbbe919a86bd3f5400172620799cf8c522a2
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2016-05-25 12:29:19 +0300

    changed duplicated man page text

--- R/cyclocomp.R
+++ R/cyclocomp.R
@@ -12,11 +12,7 @@ NULL
 
 #' Cyclomatic Complexity of R Code
 #'
-#' Cyclomatic complexity is a software metric (measurement), used to indicate
-#' the complexity of a program. It is a quantitative measure of the number of
-#' linearly independent paths through a program's source code. It was developed
-#' by Thomas J. McCabe, Sr. in 1976.
-#'
+#' Calculate the cyclomatic complexity of an R function or expression.
 #' @param expr An R function or expression.
 #' @return Integer scalar, the cyclomatic complexity of the
 #'   expression.
